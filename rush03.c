493b5eb1acbabb27b72b873518863b0b3669090e
Murat Nurkassymov <boing416@gmail.com>
2019-03-31 22:43:51 -0700

    Create rush03.c

---
+++ rush03.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnurkass <mnurkass@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/31 21:07:42 by aponomar          #+#    #+#             */
+/*   Updated: 2019/03/31 22:17:59 by mnurkass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	wrush(int x, int y)
+{
+	int	r;
+	int	c;
+
+	c = 1;
+	r = 1;
+	while (r <= y)
+	{
+		c = 1;
+		while (c <= x)
+		{
+			if ((r == 1 && c == 1) || (r == y && c == 1))
+				ft_putchar('A');
+			else if ((r == 1 && c == x && c > 1) ||
+					(r == y && c == x && c > 1))
+				ft_putchar('C');
+			else if (r > 1 && r < y && c > 1 && c < x)
+				ft_putchar(' ');
+			else
+				ft_putchar('B');
+			c++;
+		}
+		ft_putchar('\n');
+		r++;
+	}
+}
+
+void rush(int x, int y)
+{
+	if (col > 0 && row > 0)
+		wrush(col, row);
+	else
+		write(1, "enter parameters greater than zero", 34);
+}
